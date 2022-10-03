6718f9e4577fefc50a7a7c406ff1c27629c7f927
ohuo133233 <41932817+ohuo133233@users.noreply.github.com>
2022-10-03 17:01:04 +0800

    完善一版，还差移动动画

--- test/src/main/java/com/example/test/Character.java
+++ test/src/main/java/com/example/test/Character.java
@@ -2,8 +2,6 @@ package com.example.test;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,7 +16,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import java.util.ArrayList;
 
 public class Character extends ConstraintLayout {
-    private String TAG = "Character";
+    private final String TAG = "Character";
     private Context mContext;
     // 整个布局
     private View mLayout;
@@ -30,10 +28,13 @@ public class Character extends ConstraintLayout {
     private EnhanceAnimationDrawable mExecuteAnimation;
     // 等待执行的动画
     private ArrayList<EnhanceAnimationDrawable> mWaitAnimationList = new ArrayList();
-
-    private Handler mHandler;
     // 动画结束了的通知
     private int endAnimation = 1;
+
+    private int mDefaultBg;
+
+
+    private CharacterData mCharacterData;
 
     public Character(@NonNull Context context) {
         super(context);
@@ -54,24 +55,20 @@ public class Character extends ConstraintLayout {
         mContext = context;
         mLayout = LayoutInflater.from(context).inflate(R.layout.character, this, true);
         mCharacter = mLayout.findViewById(R.id.character_image);
+        initCharacterUI(R.mipmap.samurai_model_default);
 
-        mHandler = new Handler() {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                super.handleMessage(msg);
-                while (msg.what == endAnimation) {
-                    if (mWaitAnimationList.size() != 0) {
-                        Log.e(TAG, "还有多少动画在等待:" + mWaitAnimationList.size() + ", mIsAnimation: " + mIsAnimation);
-                        // 动画结束,从等待的动画list取出第一个放到执行动画的list
-                        mExecuteAnimation = mWaitAnimationList.get(0);
-                        Log.e(TAG, "1:" + mWaitAnimationList.toString());
-                        mWaitAnimationList.remove(0);
-                        executeAnimation(mExecuteAnimation);
-                    }
-                }
-            }
-        };
+        initCharacterData();
 
+    }
+
+    private void initCharacterData() {
+        mCharacterData = new CharacterData(100, 100, 100, 100, 20, 50, 10, 100, 100, 100);
+
+    }
+
+
+    public void initCharacterUI(int defaultBg) {
+        this.mDefaultBg = defaultBg;
     }
 
 
@@ -88,7 +85,60 @@ public class Character extends ConstraintLayout {
     }
 
 
+    public void skillAnimation() {
+        EnhanceAnimationDrawable skill = getSkillAnimation();
+        executeAnimation(skill);
+    }
+
+
+    public void ordinaryAttackAnimation() {
+        Log.d(TAG, "执行左的动画");
+        EnhanceAnimationDrawable ordinaryAttack = getOrdinaryAttack();
+        executeAnimation(ordinaryAttack);
+    }
+
+    private EnhanceAnimationDrawable getSkillAnimation() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(R.mipmap.male_skill_1);
+        list.add(R.mipmap.male_skill_2);
+        list.add(R.mipmap.male_skill_3);
+        list.add(R.mipmap.male_skill_4);
+        list.add(R.mipmap.male_skill_5);
+        list.add(R.mipmap.male_skill_6);
+        list.add(R.mipmap.male_skill_7);
+        list.add(R.mipmap.male_skill_8);
+
+        Resources resources = mContext.getResources();
+        EnhanceAnimationDrawable animation = new EnhanceAnimationDrawable();
+        for (int i = 0; i < list.size(); i++) {
+            animation.addFrame(resources.getDrawable(list.get(i)), 200);
+        }
+        return animation;
+
+    }
+
+    private EnhanceAnimationDrawable getOrdinaryAttack() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(R.mipmap.male_warrior_attack_1);
+        list.add(R.mipmap.male_warrior_attack_2);
+        list.add(R.mipmap.male_warrior_attack_3);
+        list.add(R.mipmap.male_warrior_attack_4);
+        list.add(R.mipmap.male_warrior_attack_5);
+
+        Resources resources = mContext.getResources();
+        EnhanceAnimationDrawable animation = new EnhanceAnimationDrawable();
+        for (int i = 0; i < list.size(); i++) {
+            animation.addFrame(resources.getDrawable(list.get(i)), 200);
+        }
+        return animation;
+
+    }
+
+
     public void leftAnimation() {
+        Log.d(TAG, "执行左的动画");
         EnhanceAnimationDrawable leftAnimation = getLeftAnimation();
         executeAnimation(leftAnimation);
     }
@@ -116,11 +166,35 @@ public class Character extends ConstraintLayout {
 
     }
 
-    public void rightAnimation() {
+    // 接口
+    private EnhanceAnimationDrawable getRightAnimation() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(R.mipmap.male_warrior_run_10);
+        list.add(R.mipmap.male_warrior_run_11);
+        list.add(R.mipmap.male_warrior_run_12);
+        list.add(R.mipmap.male_warrior_run_13);
+        list.add(R.mipmap.male_warrior_run_14);
+        list.add(R.mipmap.male_warrior_run_15);
+        list.add(R.mipmap.male_warrior_run_16);
+
+        Resources resources = mContext.getResources();
+        EnhanceAnimationDrawable animation = new EnhanceAnimationDrawable();
+        for (int i = 0; i < list.size(); i++) {
+            animation.addFrame(resources.getDrawable(list.get(i)), 200);
+        }
+        return animation;
 
     }
 
+    public void rightAnimation() {
+        Log.d(TAG, "执行右的动画");
+        EnhanceAnimationDrawable rightAnimation = getRightAnimation();
+        executeAnimation(rightAnimation);
+    }
+
     private void executeAnimation(EnhanceAnimationDrawable enhanceAnimationDrawable) {
+        Log.e(TAG, "executeAnimation: mIsAnimation: " + mIsAnimation);
         // 判断是否在进行动画
         if (mIsAnimation) {
             // 加入队列，等待执行
@@ -128,17 +202,21 @@ public class Character extends ConstraintLayout {
             mWaitAnimationList.add(enhanceAnimationDrawable);
         } else {
             mExecuteAnimation = enhanceAnimationDrawable;
+            mIsAnimation = true;
             // 没有动画，直接执行动画
             startAnimation();
         }
 
     }
 
+    private int test = 0;
+
     private void startAnimation() {
 
         mLayout.post(() -> {
             mCharacter.setBackground(null);
             mCharacter.setImageDrawable(mExecuteAnimation);
+            // 动画是否执行一次，true 表示仅执行一次，false 表示无限次循环执行动画效果
             mExecuteAnimation.setOneShot(true);
             mExecuteAnimation.start(new IAnimationDrawableCallBack() {
                 @Override
@@ -149,16 +227,25 @@ public class Character extends ConstraintLayout {
 
                 @Override
                 public void onStop() {
-
+                    Log.e(TAG, "onStop: " + mIsAnimation);
                 }
 
                 @Override
                 public void end() {
                     mIsAnimation = false;
-                    Log.e(TAG, "end: " + mIsAnimation);
-                    mHandler.sendEmptyMessage(endAnimation);
+                    test++;
                     Log.e(TAG, "动画结束");
-                    debugToast("动画结束");
+                    Log.e(TAG, "第" + test + "个动画");
+                    Log.e(TAG, "mWaitAnimationList" + mWaitAnimationList);
+                    Log.e(TAG, "mWaitAnimationList size: " + mWaitAnimationList.size());
+//                    debugToast("动画结束");
+                    setDefaultBg();
+                    // 判断是否还有要执行的动画
+                    if (mWaitAnimationList.size() != 0) {
+                        Log.e(TAG, "还有没有执行完的动画");
+                        executeAnimation(mWaitAnimationList.get(0));
+                        mWaitAnimationList.remove(0);
+                    }
                 }
             });
         });
@@ -169,4 +256,159 @@ public class Character extends ConstraintLayout {
         mLayout.post(() -> Toast.makeText(mContext, message, Toast.LENGTH_LONG).show());
     }
 
+
+    public void setDefaultBg() {
+        mCharacter.post(() -> {
+            mCharacter.setImageDrawable(null);
+            mCharacter.setBackgroundResource(mDefaultBg);
+        });
+
+    }
+
+    public void ordinaryAttack(Character enemyCharacter) {
+        // 动画效果
+        leftAnimation();
+        ordinaryAttackAnimation();
+        rightAnimation();
+
+        // 获取普通攻击伤害
+        long ordinaryAttack = mCharacterData.getOrdinaryAttack();
+        Log.e(TAG, "我的普通攻击: " + ordinaryAttack);
+        long defense = enemyCharacter.mCharacterData.getDefense();
+        Log.e(TAG, "敌人的防御: " + defense);
+
+        // 造成的伤害
+        long injuries = ordinaryAttack - defense;
+        // 至少造成1点伤害
+        if (injuries < 1) {
+            injuries = 1;
+        }
+
+        // 获取敌人当前血量
+        long current_hp = enemyCharacter.mCharacterData.getCurrent_hp();
+        // 当前血量减去当前受到的伤害
+        long enemyCurrentHp = current_hp - injuries;
+
+        // 防止血量成为负值
+        if (enemyCurrentHp < 0) {
+            enemyCurrentHp = 0;
+        }
+
+        enemyCharacter.mCharacterData.setCurrent_hp(enemyCurrentHp);
+        Log.e(TAG,"攻击完，敌人剩余的HP: "+enemyCharacter.mCharacterData.getCurrent_hp());
+    }
+
+    private static class CharacterData {
+        // 最大血量
+        private long hp;
+        // 最大魔法值
+        private long mp;
+        // 当前HP
+        private long current_hp;
+        // 当前魔法值
+        private long current_mp;
+        // 普通攻击力
+        private long ordinaryAttack;
+        // 速度
+        private long speed;
+        // 防御
+        private long defense;
+        // 技能伤害
+        private long skill_one;
+        private long skill_two;
+        private long skill_three;
+
+        public CharacterData(long hp, long mp, long current_hp, long current_mp, long ordinaryAttack, long speed, long defense, long skill_one, long skill_two, long skill_three) {
+            this.hp = hp;
+            this.mp = mp;
+            this.current_hp = current_hp;
+            this.current_mp = current_mp;
+            this.ordinaryAttack = ordinaryAttack;
+            this.speed = speed;
+            this.defense = defense;
+            this.skill_one = skill_one;
+            this.skill_two = skill_two;
+            this.skill_three = skill_three;
+        }
+
+        public long getHp() {
+            return hp;
+        }
+
+        public void setHp(long hp) {
+            this.hp = hp;
+        }
+
+        public long getMp() {
+            return mp;
+        }
+
+        public void setMp(long mp) {
+            this.mp = mp;
+        }
+
+        public long getCurrent_hp() {
+            return current_hp;
+        }
+
+        public void setCurrent_hp(long current_hp) {
+            this.current_hp = current_hp;
+        }
+
+        public long getCurrent_mp() {
+            return current_mp;
+        }
+
+        public void setCurrent_mp(long current_mp) {
+            this.current_mp = current_mp;
+        }
+
+        public long getOrdinaryAttack() {
+            return ordinaryAttack;
+        }
+
+        public void setOrdinaryAttack(long ordinaryAttack) {
+            this.ordinaryAttack = ordinaryAttack;
+        }
+
+        public long getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(long speed) {
+            this.speed = speed;
+        }
+
+        public long getDefense() {
+            return defense;
+        }
+
+        public void setDefense(long defense) {
+            this.defense = defense;
+        }
+
+        public long getSkill_one() {
+            return skill_one;
+        }
+
+        public void setSkill_one(long skill_one) {
+            this.skill_one = skill_one;
+        }
+
+        public long getSkill_two() {
+            return skill_two;
+        }
+
+        public void setSkill_two(long skill_two) {
+            this.skill_two = skill_two;
+        }
+
+        public long getSkill_three() {
+            return skill_three;
+        }
+
+        public void setSkill_three(long skill_three) {
+            this.skill_three = skill_three;
+        }
+    }
 }
--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,44 +14,17 @@ public class MainActivity extends AppCompatActivity {
         ImageView test_image = findViewById(R.id.test_image);
 
         Character character = findViewById(R.id.character);
-        character.leftAnimation();
-        character.leftAnimation();
-        character.leftAnimation();
-//        character.leftAnimation();
-//        character.leftAnimation();
 
-//        EnhanceAnimationDrawable enhanceAnimationDrawable = new EnhanceAnimationDrawable();
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_1), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_2), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_3), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_4), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_5), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_6), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_7), 200);
-//        enhanceAnimationDrawable.addFrame(getResources().getDrawable(R.mipmap.male_warrior_run_8), 200);
-//        enhanceAnimationDrawable.setOneShot(true);
-//        test_image.setBackground(null);
-//        test_image.setImageDrawable(enhanceAnimationDrawable);
-//        enhanceAnimationDrawable.start(new IAnimationDrawableCallBack() {
-//            @Override
-//            public void onStart() {
+        character.ordinaryAttack(character);
+
+//        character.leftAnimation();
+//        character.leftAnimation();
 //
-//            }
+//        character.ordinaryAttackAnimation();
+//        character.skillAnimation();
 //
-//            @Override
-//            public void onStop() {
-//
-//            }
-//
-//            @Override
-//            public void end() {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(MainActivity.this, "动画结束", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
+//        character.rightAnimation();
+//        character.rightAnimation();
+
     }
 }
